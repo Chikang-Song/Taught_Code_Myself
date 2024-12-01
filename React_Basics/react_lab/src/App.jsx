51fea8a2a3134b850ec1117d14af375c7858efb7
Chikang-Song <haemyeome@gmail.com>
2024-12-01 17:12:49 +0900

    Passing props 실습 자료

---
+++ React_Basics/react_lab/src/App.jsx
@@ -0,0 +1,12 @@
+import Heading from "./Heading.jsx";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstname="Any name other than Bob"/>
+            <Heading firstname="Jack"/>
+        </div>
+    );
+};
+
+export default App;
